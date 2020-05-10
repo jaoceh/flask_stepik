13eac0533713c503f22d88eb64fe4a8889c245b0
Matvey Tykheev <matvey.tykheev@bostongene.com>
2020-05-10 18:48:08 +0300

    Доделал MVP

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template    # сперва подключим модуль
 from flask_debugtoolbar import DebugToolbarExtension
-from data import tours, departures
+import data
+from random import randint
 
 app = Flask(__name__)      # объявим экземпляр фласка
 
@@ -11,20 +12,33 @@ app.config['SECRET_KEY'] = '183-618-819'
 
 toolbar = DebugToolbarExtension(app)
 
+
 @app.route('/')
 def main():
-    return render_template('index.html')
+    pinned_tours = {}
+    while len(pinned_tours) < 6:
+        id = randint(1,16)
+        pinned_tours[id] = data.tours[id]
+    return render_template('index.html', subtitle=data.subtitle, title=data.title, description=data.description, pinned_tours=pinned_tours, departures=data.departures)
 
 @app.route('/departures/<departure>')
 def departure(departure):
-    return render_template('departure.html')
+    departure_tours = {}
+    departure_tour_prices = []
+    departure_tour_nights = []
+    for k, v in data.tours.items():
+        if v['departure'] == departure:
+            departure_tours[k]=v
+            departure_tour_prices.append(v['price'])
+            departure_tour_nights.append(v['nights'])
+    return render_template('departure.html', title=data.title, departure_tours = departure_tours, departures=data.departures, departure_tour_prices=departure_tour_prices, departure_tour_nights=departure_tour_nights)
 
 @app.route('/tours/<id>')
 def tour(id):
     id = int(id)
-    departure = departures[tours[id]['departure']]
-    stars=int(tours[id]['stars'])
-    return render_template('tour.html', tour=tours[id], departure=departure, stars=stars)
+    tour = data.tours[id]
+    departure = data.departures[tour['departure']]
+    return render_template('tour.html', tour=tour, departure=departure, title=data.title, departures=data.departures)
 
 
 
